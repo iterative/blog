67c853fbf34a9d1719f458bd1fcf092cf15e1cfc
Restyled.io <commits@restyled.io>
2019-12-29 02:17:45 +0000

    Restyled by prettier

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,32 +1,32 @@
 const fs = require("fs");
 const path = require("path");
-const {createFilePath} = require("gatsby-source-filesystem");
-const {siteMetadata} = require("./gatsby-config");
+const { createFilePath } = require("gatsby-source-filesystem");
+const { siteMetadata } = require("./gatsby-config");
 
-exports.createPages = async ({graphql, actions}) => {
-  const {createPage} = actions;
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions;
 
   const blogPost = path.resolve("./src/templates/blog-post.tsx");
   const result = await graphql(`
-      {
-        allMarkdownRemark(
-          sort: { fields: [frontmatter___date], order: DESC }
-          filter: { fileAbsolutePath: { regex: "/content/blog/" } }
-          limit: 1000
-        ) {
-          edges {
-            node {
-              fields {
-                slug
-              }
-              frontmatter {
-                title
-              }
+    {
+      allMarkdownRemark(
+        sort: { fields: [frontmatter___date], order: DESC }
+        filter: { fileAbsolutePath: { regex: "/content/blog/" } }
+        limit: 1000
+      ) {
+        edges {
+          node {
+            fields {
+              slug
+            }
+            frontmatter {
+              title
             }
           }
         }
       }
-    `);
+    }
+  `);
 
   if (result.errors) {
     throw result.errors;
@@ -40,25 +40,25 @@ exports.createPages = async ({graphql, actions}) => {
     const next = index === 0 ? null : posts[index - 1].node;
 
     createPage({
-      path : post.node.fields.slug,
-      component : blogPost,
-      context : {slug : post.node.fields.slug, previous, next}
+      path: post.node.fields.slug,
+      component: blogPost,
+      context: { slug: post.node.fields.slug, previous, next }
     });
   });
 };
 
-exports.onCreateNode = ({node, actions, getNode}) => {
-  const {createNodeField} = actions;
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions;
 
   if (node.internal.type === "MarkdownRemark") {
-    const value = createFilePath({node, getNode}).replace(/^\/[0-9\-]*/, "/");
-    createNodeField({name : "slug", node, value});
+    const value = createFilePath({ node, getNode }).replace(/^\/[0-9\-]*/, "/");
+    createNodeField({ name: "slug", node, value });
   }
 };
 
 // Create json to use on https://dvc.org/community
 
-exports.onPostBuild = async function({graphql}) {
+exports.onPostBuild = async function({ graphql }) {
   const result = await graphql(`
     {
       allMarkdownRemark(
@@ -87,20 +87,23 @@ exports.onPostBuild = async function({graphql}) {
   }
 
   const posts = result.data.allMarkdownRemark.edges.map(
-      ({
-        node : {fields : {slug}, frontmatter : {title, date, commentsUrl}}
-      }) => {
-        const url = `${siteMetadata.siteUrl}/${slug}`;
+    ({
+      node: {
+        fields: { slug },
+        frontmatter: { title, date, commentsUrl }
+      }
+    }) => {
+      const url = `${siteMetadata.siteUrl}/${slug}`;
 
-        return {url, title, date, commentsUrl};
-      });
+      return { url, title, date, commentsUrl };
+    }
+  );
 
   const dir = path.join(__dirname, "/public/api");
   const filepath = path.join(dir, "posts.json");
 
   // Write json file to the public dir,
   // it will be used community page later
-  if (!fs.existsSync(dir))
-    fs.mkdirSync(dir);
-  fs.writeFileSync(filepath, JSON.stringify({posts}));
+  if (!fs.existsSync(dir)) fs.mkdirSync(dir);
+  fs.writeFileSync(filepath, JSON.stringify({ posts }));
 };
